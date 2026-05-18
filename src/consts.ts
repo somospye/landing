4843d071d43ee201adcd57b07a4b66b6de32caa0
LDarki <51938450+LDarki@users.noreply.github.com>
2026-05-18 14:33:51 -0300

    chore: format (add biomejs and lefthook)

--- src/consts.ts
+++ src/consts.ts
@@ -1,53 +1,53 @@
-import type { SvgComponent } from "astro/types";
-import XLogo from "./assets/icons/x.svg";
-import YoutubeLogo from "./assets/icons/youtube.svg";
-import DiscordLogo from "./assets/icons/discord.svg";
-import InstagramLogo from "./assets/icons/instagram.svg";
+import type { SvgComponent } from 'astro/types';
+import DiscordLogo from './assets/icons/discord.svg';
+import InstagramLogo from './assets/icons/instagram.svg';
+import XLogo from './assets/icons/x.svg';
+import YoutubeLogo from './assets/icons/youtube.svg';
 
-export const PYETIMES_URL = "https://pyetimes.com";
-export const GITHUB_URL = "https://github.com/somospye";
-export const DISCORD_INVITE_URL = "https://discord.gg/programacion";
-export const TWITTER_URL = "https://x.com/somospye";
-export const INSTAGRAM_URL = "https://www.instagram.com/somospye";
-export const YOUTUBE_URL = "https://www.youtube.com/@somospye";
+export const PYETIMES_URL = 'https://pyetimes.com';
+export const GITHUB_URL = 'https://github.com/somospye';
+export const DISCORD_INVITE_URL = 'https://discord.gg/programacion';
+export const TWITTER_URL = 'https://x.com/somospye';
+export const INSTAGRAM_URL = 'https://www.instagram.com/somospye';
+export const YOUTUBE_URL = 'https://www.youtube.com/@somospye';
 
 interface Link {
-    name: string;
-    href: string;
+	name: string;
+	href: string;
 }
 
 export const NAVIGATION_LINKS: Link[] = [
-    { name: "Unirme", href: DISCORD_INVITE_URL },
-    { name: "Contactos", href: "#contact" },
-    { name: "GitHub", href: GITHUB_URL },
-    { name: "PyE Times", href: PYETIMES_URL },
+	{ name: 'Unirme', href: DISCORD_INVITE_URL },
+	{ name: 'Contactos', href: '#contact' },
+	{ name: 'GitHub', href: GITHUB_URL },
+	{ name: 'PyE Times', href: PYETIMES_URL },
 ];
 
 interface SocialLink {
-    name: string;
-    href: string;
-    icon: SvgComponent;
+	name: string;
+	href: string;
+	icon: SvgComponent;
 }
 
 export const SOCIAL_LINKS: SocialLink[] = [
-    {
-        name: "Discord",
-        href: DISCORD_INVITE_URL,
-        icon: DiscordLogo,
-    },
-    {
-        name: "X",
-        href: TWITTER_URL,
-        icon: XLogo,
-    },
-    {
-        name: "YouTube",
-        href: YOUTUBE_URL,
-        icon: YoutubeLogo,
-    },
-    {
-        name: "Instagram",
-        href: INSTAGRAM_URL,
-        icon: InstagramLogo,
-    },
+	{
+		name: 'Discord',
+		href: DISCORD_INVITE_URL,
+		icon: DiscordLogo,
+	},
+	{
+		name: 'X',
+		href: TWITTER_URL,
+		icon: XLogo,
+	},
+	{
+		name: 'YouTube',
+		href: YOUTUBE_URL,
+		icon: YoutubeLogo,
+	},
+	{
+		name: 'Instagram',
+		href: INSTAGRAM_URL,
+		icon: InstagramLogo,
+	},
 ];
